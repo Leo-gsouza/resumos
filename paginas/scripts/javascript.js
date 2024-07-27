a2249eb9013bf10d0b098bfe4ed111708e03de41
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-07-27 13:40:32 -0300

    alterando css das respostas

--- paginas/scripts/javascript.js
+++ paginas/scripts/javascript.js
@@ -4,12 +4,14 @@ function ver(resolucao){
 
 function ex001(res){
     let numero = document.getElementById('numeroId').value
+    res.style.display = 'block'
     res.innerHTML = `Valor inteiro é ${parseInt(numero)}`
 }
 
 function ex002(opo, adj, res){
     let adjacente = Number(adj.value)
     let oposto = Number(opo.value)
+    res.style.display = 'block'
     res.innerHTML = `Hipotenusa = ${((oposto**2 + adjacente**2) **0.5).toFixed(2)}`
 }
 
@@ -20,6 +22,7 @@ function ex003(valor, res){
     let seno = radianos - (radianos**3/6) + (radianos**5/120) - (radianos**7/5040)
     let cosseno = 1 - (radianos**2/2) + (radianos**4/24) - (radianos**6/720)
     let tangente = seno/cosseno
+    res.style.display = 'block'
 
 
     res.innerHTML =  `Seno = ${seno.toFixed(3)}<br>Cosseno = ${cosseno.toFixed(3)}<br>Tangente = ${tangente.toFixed(3)}`
@@ -30,6 +33,7 @@ function ex004(resultado){
     const aluno2 = document.getElementById('aluno2Id').value
     const aluno3 = document.getElementById('aluno3Id').value
     const aluno4 = document.getElementById('aluno4Id').value
+    resultado.style.display = 'block'
 
     const alunos = []
     if (aluno1) alunos.push(aluno1)
@@ -49,6 +53,7 @@ function ex005(resultado){
     const aluno2 = document.getElementById('aluno2Id').value
     const aluno3 = document.getElementById('aluno3Id').value
     const aluno4 = document.getElementById('aluno4Id').value
+    resultado.style.display = 'block'
 
     const alunos = []
     if (aluno1) alunos.push(aluno1)
@@ -70,6 +75,7 @@ function ex006(resultado){
     const minusculo = nome.toLowerCase()
     const espacos = nome.split(' ').length -1
     const caracteres = nome.length - espacos
+    resultado.style.display = 'block'
 
     resultado.innerHTML = `Primeiro nome: ${primeiro} \
     <br>Ultimo nome: ${ultimo} <br>Maiusculo: ${maiusculo}\
@@ -82,6 +88,7 @@ function ex007(resultado){
     let dezena = Math.floor(numero / 10) % 10
     let centena = Math.floor(numero / 100) % 10
     let milhar = Math.floor(numero/1000) % 10
+    resultado.style.display = 'block'
 
     resultado.innerHTML = `Unidade = ${unidade}\
     <br>Dezena = ${dezena}\
@@ -91,17 +98,51 @@ function ex007(resultado){
 
 function ex008(cid, resultado){
     let cidade = String(cid.value)
-    if (cidade.substring(0,5).toUpperCase() == 'SANTO'){
+    resultado.style.display = 'block'
+    if (cidade.substring(0,6).toUpperCase() == 'SANTO '){
         resultado.innerHTML = `Sim! a cidade começa com Santo`
     }else{resultado.innerHTML = `A cidade não começa com Santo`}
 }
 
 function ex009(name, resultado){
+    let nome = String(name.value).toUpperCase()
+    let separado = nome.split(" ")
+    let ultimo = separado[separado.length - 1]
+    resultado.style.display = 'block'
+
+    if (ultimo == 'SILVA'){
+        resultado.innerHTML = `Sim! Tem Silva no nome`
+    }else{
+        if (nome.includes(' SILVA ')){
+            resultado.innerHTML = `Sim! Tem Silva no nome`
+        }else{
+            resultado.innerHTML = `Não tem Silva no nome`
+        }
+    }
+}
+
+function ex010(resultado){
+    let frase = document.getElementById('fraseId10').value.toLowerCase()
+    frase = frase.replace(/[âáãà]/g, 'a')
+    frase = frase.replace(/ /g, '')
+    let letraA = frase.split('a').length - 1
+    let primeira = frase.indexOf('a') + 1
+    let ultima = frase.lastIndexOf('a') + 1 
+    resultado.style.display = 'block'
+
+    resultado.innerHTML = `<p>Quantidade de letras 'a' = ${letraA}</p>\
+    <p>1ª letra 'a' está na posição ${primeira}</p>\
+    <p>Ultima letra 'a' está na posição ${ultima}</p>` 
+}
+
+
+ function ex011(name, resultado){
     let nome = String(name.value)
     let separado = nome.split(" ")
     let primeiro = separado[0]
-    let ultimo = separado[separado.length - 1]
+    let ultimo = separado[separado.length - 1]  
+    resultado.style.display = 'block'
 
-    resultado.innerHTML = `Primeiro nome = ${primeiro}\
-    <br>Ultimo nome: ${ultimo}`
-}
+    resultado.innerHTML = `<p>Primeiro: ${primeiro}</p>\
+    <p>Ultimo: ${ultimo}</p>`
+ }
